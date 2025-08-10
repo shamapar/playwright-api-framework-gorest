ce15eeb515d7221cbe5f02edad9c72beeb56b291
Shama <shamapar494@gmail.com>
2025-08-10 21:07:29 +1000

    fixed token error

--- readme.md
+++ readme.md
@@ -20,10 +20,6 @@ npm install
 ```
 create a unique token from https://gorest.co.in/ and add it into tests\helper\env.ts authorization key
 ```
-### Install browser for report generate
-```
-npm i -D @playwright/browser-chromium
-```
 
 ### Run test
 ```
--- tests/helper/env.ts
+++ tests/helper/env.ts
@@ -1,4 +1,6 @@
 export const env = {
     baseurl: "https://gorest.co.in",
-    Headers: { "Authorization": "Bearer replace-with-generated-unique-token-key }
+    Headers: {
+        "Authorization": "Bearer replace-with-generated-unique-token-key"
+    }
 }
